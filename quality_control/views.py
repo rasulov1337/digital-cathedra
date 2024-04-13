7dad5eaf44a2ccb3a04b3c11667befd14c99e5f5
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-04-13 23:38:36 +0300

    Finish the 6th lecture

--- quality_control/views.py
+++ quality_control/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse
 
-# Create your views here.
+
+def index(request):
+    bugs_page_url = reverse('quality_control:bugs')
+    feature_list_url = reverse('quality_control:features')
+
+    html = (f"<h1>Система контроля качества</h1>"
+            f"<a href='{bugs_page_url}'>Список всех багов</a><br>"
+            f"<a href='{feature_list_url}'>Запросы на улучшение</a>")
+    return HttpResponse(html)
+
+
+def bug_list(request):
+    return HttpResponse("Cписок отчетов об ошибках")
+
+
+def feature_list(request):
+    return HttpResponse("Список запросов на улучшение")
+
+
+def bug_detail(request, bug_id):
+    html = (f"<h1>Детали бага {bug_id}</h1>")
+    return HttpResponse(html)
+
+
+def feature_id_detail(request, feature_id):
+    html = (f"<h1>Детали улучшения {feature_id}</h1>")
+    return HttpResponse(html)
