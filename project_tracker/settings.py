690e14bd2b459b3f44a4d0c4f44561feb933d30a
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-04-14 12:03:15 +0300

    Finish the 8th lecture

--- project_tracker/settings.py
+++ project_tracker/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # наше приложение tasks
+    # наше приложение quality_control
     'tasks.apps.TasksConfig',
     'quality_control.apps.QualityControlConfig'
 ]
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'project_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- quality_control/views.py
+++ quality_control/views.py
@@ -1,38 +1,28 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import View, DetailView
 
 from quality_control.models import BugReport, FeatureRequest
 
 
-def index(request):
-    bugs_page_url = reverse('quality_control:bugs')
-    feature_list_url = reverse('quality_control:features')
-
-    html = (f"<h1>Система контроля качества</h1>"
-            f"<a href='{bugs_page_url}'>Список всех багов</a><br>"
-            f"<a href='{feature_list_url}'>Запросы на улучшение</a>")
-    return HttpResponse(html)
+# def index(request):
+#     bugs_page_url = reverse('quality_control:bugs')
+#     feature_list_url = reverse('quality_control:features')
+#
+#     html = (f"<h1>Система контроля качества</h1>"
+#             f"<a href='{bugs_page_url}'>Список всех багов</a><br>"
+#             f"<a href='{feature_list_url}'>Запросы на улучшение</a>")
+#     return HttpResponse(html)
 
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        bugs_page_url = reverse('quality_control:bugs')
-        feature_list_url = reverse('quality_control:features')
-
-        html = (f"<h1>Система контроля качества</h1>"
-                f"<a href='{bugs_page_url}'>Список всех багов</a><br>"
-                f"<a href='{feature_list_url}'>Запросы на улучшение</a>")
-        return HttpResponse(html)
+        return render(request, 'quality_control/index.html')
 
 
 def bug_list(request):
-    html = '<h1>Список отчетов об ошибках</h1>'
-    for i in BugReport.objects.all():
-        url = reverse('quality_control:bug_detail', args=[i.id])
-
-        html += f'<li><a href={url}>{i}</a></li>'
-    return HttpResponse(html)
+    return render(request, 'quality_control/bug_list.html', {'bug_reports': BugReport.objects.all()})
 
 
 class BugDetailView(DetailView):
@@ -43,18 +33,12 @@ class BugDetailView(DetailView):
         self.object = self.get_object()
         bug = self.object
 
-        html = (f'<h1>Title: {bug.title}<br>Description: {bug.description}<br>Status: {bug.status}'
-                f'<br>Priority: {bug.priority}</h1>')
-        return HttpResponse(html)
+        return render(request, 'quality_control/bug_detail.html', {'bug': bug})
 
 
 def feature_list(request):
-    html = '<h1>Список запросов на улучшение</h1>'
-    for i in FeatureRequest.objects.all():
-        url = reverse('quality_control:feature_id_detail', args=[i.id])
-
-        html += f'<li><a href={url}>{i}</a></li>'
-    return HttpResponse(html)
+    return render(request, 'quality_control/feature_list.html',
+                  {'feature_list': FeatureRequest.objects.all()})
 
 
 # def bug_detail(request, bug_id):
@@ -75,6 +59,4 @@ class FeatureDetailView(DetailView):
         self.object = self.get_object()
         feature = self.object
 
-        html = (f'<h1>Title: {feature.title}<br>Description: {feature.description}<br>Status: {feature.status}'
-                f'<br>Priority: {feature.priority}<br>Project: {feature.project}<br>Task: {feature.task}</h1>')
-        return HttpResponse(html)
+        return render(request, 'quality_control/feature_detail.html', {'feature': feature})
